431bf6b059dec7649618a2cefa7b60eb445ac658
Miguel <miguel.ramirez@udea.edu.co>
2021-05-14 12:56:15 -0500

    mongoreactivo

---
+++ src/main/java/com/example/demo/user/Score.java
@@ -0,0 +1,22 @@
+package com.example.demo.user;
+
+public class Score {
+    Double score;
+    String type;
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
